51f567561510aa9ebce5b88a433f5fa064a9a151
huyldph <114159050+huyldph@users.noreply.github.com>
2025-04-18 10:11:06 +0700

    feat(api-gateway): add config cors

--- api-gateway/src/main/java/com/devteria/gateway/configuration/WebClientConfiguration.java
+++ api-gateway/src/main/java/com/devteria/gateway/configuration/WebClientConfiguration.java
@@ -3,9 +3,14 @@ package com.devteria.gateway.configuration;
 import com.devteria.gateway.repository.IdentityClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.util.List;
 
 @Configuration
 public class WebClientConfiguration {
@@ -23,5 +28,18 @@ public class WebClientConfiguration {
                 .build();
 
         return httpServiceProxyFactory.createClient(IdentityClient.class);
+    }
+
+    @Bean
+    CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsWebFilter(source);
     }
 }
