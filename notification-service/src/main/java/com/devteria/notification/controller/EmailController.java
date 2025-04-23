02b565f7f98fb2b6ba12397ec2c5382334a9cbd1
huyldph <ledinhhuy26032004@gmail.com>
2025-04-23 20:07:56 +0700

    feat(notification-service): add EmailController to expose email sending API

---
+++ notification-service/src/main/java/com/devteria/notification/controller/EmailController.java
@@ -0,0 +1,34 @@
+package com.devteria.notification.controller;
+
+import com.devteria.notification.dto.ApiResponse;
+import com.devteria.notification.dto.request.SendEmailRequest;
+import com.devteria.notification.dto.response.EmailResponse;
+import com.devteria.notification.service.EmailService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Log4j2
+public class EmailController {
+    EmailService emailService;
+
+    @PostMapping("/email/send")
+    ApiResponse<EmailResponse> sendEmail(@RequestBody SendEmailRequest request){
+        return ApiResponse.<EmailResponse>builder()
+                .result(emailService.sendEmail(request))
+                .build();
+    }
+
+    @KafkaListener(topics = "onboard-successful")
+    public void listen(String message){
+        log.info("Message received: {}", message);
+    }
+}
