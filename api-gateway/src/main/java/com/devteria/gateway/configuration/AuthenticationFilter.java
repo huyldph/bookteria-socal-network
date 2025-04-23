9c000bbc8f2d9f2efce6aa4b8a6897e30bce9e09
huyldph <ledinhhuy26032004@gmail.com>
2025-04-23 20:10:38 +0700

    feat(api-gateway): add public endpoint for email sending

--- api-gateway/src/main/java/com/devteria/gateway/configuration/AuthenticationFilter.java
+++ api-gateway/src/main/java/com/devteria/gateway/configuration/AuthenticationFilter.java
@@ -35,7 +35,11 @@ public class AuthenticationFilter implements GlobalFilter, Ordered {
     ObjectMapper objectMapper;
 
     @NonFinal
-    private String[] publicEndpoints = {"/identity/auth/.*", "/identity/users/registration"};
+    private String[] publicEndpoints = {
+            "/identity/auth/.*",
+            "/identity/users/registration",
+            "/notification/email/send"
+    };
 
     @NonFinal
     @Value("${app.api-prefix}")
