f17a7c46fad71485a930acf1608c9483e52fdb21
huyldph <ledinhhuy26032004@gmail.com>
2025-05-11 18:52:13 +0700

    feat(web-app): update ui

--- web-app/src/configurations/configuration.js
+++ web-app/src/configurations/configuration.js
@@ -1,9 +1,10 @@
 export const CONFIG = {
-  API_GATEWAY: "http://localhost:8888/api/v1",
+    API_GATEWAY: "http://localhost:8888/api/v1",
 };
 
 export const API = {
-  LOGIN: "/identity/auth/token",
-  MY_INFO: "/profile/users/my-profile",
-  MY_POST: "/post/my-posts",
+    LOGIN: "/identity/auth/token",
+    MY_INFO: "/profile/users/my-profile",
+    MY_POST: "/post/my-posts",
+    UPDATE_PROFILE: "/profile/users/update-profile",
 };
